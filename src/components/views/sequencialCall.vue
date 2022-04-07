a1b13c937f24c4e015a23e36891ac57cbd8f0c11
youngmin.cho <holic158@naver.com>
2022-04-07 18:47:33 +0900

    javascript mulitple then 처리

--- src/components/views/sequencialCall.vue
+++ src/components/views/sequencialCall.vue
@@ -23,7 +23,8 @@ export default ({
       let time = new Date().toISOString();
       console.log('2. sequencialCallTest', time);
       this.getAleBeer()
-        .then(this.getCoffee);
+        .then(this.getCoffee)
+        .then(this.getCoffee2);
     },
     /**
      * Get Ale Beer
@@ -34,11 +35,11 @@ export default ({
       return axios.get('https://api.sampleapis.com/beers/ale')
         .then(res => {
           time = new Date().toISOString();
-          console.log('4.1 getAleBeer Success', res, time);
+          console.log('3.1 getAleBeer Success', res, time);
         })
         .catch((error) => {
           time = new Date().toISOString();
-          console.log('4.2 getAleBeer Fail', error, time);
+          console.log('3.2 getAleBeer Fail', error, time);
           throw error;
         })
     },
@@ -47,16 +48,31 @@ export default ({
      */
     getCoffee () {
       let time = new Date().toISOString();
-      console.log('5. getCoffee Called', time);
+      console.log('4. getCoffee Called', time);
       axios.get('https://api.sampleapis.com/coffee/hot')
         .then(res => {
           time = new Date().toISOString();
-          console.log('6. getCoffee Success', res, time);
+          console.log('4.1 getCoffee Success', res, time);
         })
         .catch((error) => {
           console.log(error);
         })
-    }
+    },
+    /**
+     * Get Coffee
+     */
+    getCoffee2 () {
+      let time = new Date().toISOString();
+      console.log('5. getCoffee Called2', time);
+      axios.get('https://api.sampleapis.com/coffee/hot')
+        .then(res => {
+          time = new Date().toISOString();
+          console.log('5.1 getCoffee Success', res, time);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+    }    
   }
 })
 </script>
