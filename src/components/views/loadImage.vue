331802b67c0e8487d1eec984e1d2effda8b75ca8
youngmin.cho <holic158@naver.com>
2022-05-06 17:46:57 +0900

    load Img Samples

--- src/components/views/loadImage.vue
+++ src/components/views/loadImage.vue
@@ -2,91 +2,44 @@
     <div>
         <input type="file" @change="changImage($event)">
     </div>
+    <div style="padding-top:20px">
+      <textarea v-model="message" style="width:100%;height:500px"></textarea>
+    </div>
 </template>
 <script>
 import EXIF from 'exif-js'
-// import loadImage from 'blueimp-load-image'
-import axios from 'axios';
 
 export default ({
-  name: 'sequencialCall',
+  name: 'loadImage',
   data () {
     return {
-
+      message: 'Vue exif meta info',
     }
   },
   created () {
-    let time = new Date().toISOString();
-    console.log('1. created called', time);
-    // this.sequencialCallTest();
+    console.log('this.message', this.message);
+    this.test();
   },
   methods: {
     changImage(e) {
       console.log('onChange!')
+      const vm = this;
       if (e) {
           // eslint-disable-next-line no-undef
           EXIF.getData(e.target.files[0], function() {
               console.log('image info', this)
               console.log('exif data', this.exifdata)
+              vm.message = JSON.stringify(this.exifdata);
+              // vm.test();
           })
       } else {
           console.log(`it's not image`)
       }
     },
-    sequencialCallTest () {
-      let time = new Date().toISOString();
-      console.log('2. sequencialCallTest', time);
-      this.getAleBeer()
-        .then(this.getCoffee)
-        .then(this.getCoffee2);
+    test() {
+      // console.log('test called ');
+      // this.message = 'after test2';
     },
-    /**
-     * Get Ale Beer
-     */
-    getAleBeer () {
-      let time = new Date().toISOString();
-      console.log('3. getAleBeer Called', time);
-      return axios.get('https://api.sampleapis.com/beers/ale')
-        .then(res => {
-          time = new Date().toISOString();
-          console.log('3.1 getAleBeer Success', res, time);
-        })
-        .catch((error) => {
-          time = new Date().toISOString();
-          console.log('3.2 getAleBeer Fail', error, time);
-          throw error;
-        })
-    },
-    /**
-     * Get Coffee
-     */
-    getCoffee () {
-      let time = new Date().toISOString();
-      console.log('4. getCoffee Called', time);
-      axios.get('https://api.sampleapis.com/coffee/hot')
-        .then(res => {
-          time = new Date().toISOString();
-          console.log('4.1 getCoffee Success', res, time);
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-    },
-    /**
-     * Get Coffee
-     */
-    getCoffee2 () {
-      let time = new Date().toISOString();
-      console.log('5. getCoffee Called2', time);
-      axios.get('https://api.sampleapis.com/coffee/hot')
-        .then(res => {
-          time = new Date().toISOString();
-          console.log('5.1 getCoffee Success', res, time);
-        })
-        .catch((error) => {
-          console.log(error);
-        })
-    }    
   }
 })
 </script>
