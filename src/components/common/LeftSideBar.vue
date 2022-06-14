4b9451afb9f8513eabb2706a1435b0c0b2054b0b
youngmin.cho <holic158@naver.com>
2022-06-14 10:53:44 +0900

    사이드바 구성

--- src/components/common/LeftSideBar.vue
+++ src/components/common/LeftSideBar.vue
@@ -1,12 +1,50 @@
 <template>
 <aside class="sidebar">
     <nav class="nav-links nav">
-        <div class="item">
-            <div class="nav-link">
-                <a class="item active" href="/guide/"></a>
-            </div>
-        </div>
     </nav>
+    <ul class="sidebar-links">
+        <li class="sidebar-link">
+            <a class="sidebar-link-item" href="/introduction.html">Introduction</a>
+        </li>
+        <li class="sidebar-link">
+            <a class="sidebar-link-item" href="/installation.html">Installation</a>
+        </li>
+        <li class="sidebar-link">
+            <p class="sidebar-link-item">Essentials</p>
+            <ul class="sidebar-links">
+                <li class="sidebar-link">
+                    <a class="sidebar-link-item active" href="/guide/">Getting Started</a>
+                    <ul class="sidebar-links">
+                        <li class="sidebar-link">
+                            <a class="sidebar-link-item" href="#html">HTML</a>
+                                <ul class="sidebar-links">
+                                    <li class="sidebar-link"><a class="sidebar-link-item" href="#router-link">router-link</a><!---->
+                                    </li>
+                                    <li class="sidebar-link"><a class="sidebar-link-item" href="#router-view">router-view</a><!---->
+                                    </li>
+                                </ul>
+                        </li>
+                        <li class="sidebar-link">
+                            <a class="sidebar-link-item" href="#javascript">JavaScript</a><!---->
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-link">
+                    <a class="sidebar-link-item" href="/guide/essentials/dynamic-matching.html">Dynamic Route Matching</a>
+                </li>
+                <li class="sidebar-link">
+                    <a class="sidebar-link-item" href="/guide/essentials/route-matching-syntax.html">Routes' Matching Syntax</a>
+                </li>
+                <li class="sidebar-link"><a class="sidebar-link-item" href="/guide/essentials/nested-routes.html">Nested Routes</a><!----></li>
+                <li class="sidebar-link"><a class="sidebar-link-item" href="/guide/essentials/navigation.html">Programmatic Navigation</a><!----></li>
+                <li class="sidebar-link"><a class="sidebar-link-item" href="/guide/essentials/named-routes.html">Named Routes</a><!----></li>
+                <li class="sidebar-link"><a class="sidebar-link-item" href="/guide/essentials/named-views.html">Named Views</a><!----></li>
+                <li class="sidebar-link"><a class="sidebar-link-item" href="/guide/essentials/redirect-and-alias.html">Redirect and Alias</a><!----></li>
+                <li class="sidebar-link"><a class="sidebar-link-item" href="/guide/essentials/passing-props.html">Passing Props to Route Components</a><!----></li>
+                <li class="sidebar-link"><a class="sidebar-link-item" href="/guide/essentials/history-mode.html">Different History modes</a><!----></li>
+            </ul>
+        </li>        
+    </ul>
 </aside>
 </template>
 
